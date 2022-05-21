ad5cca119ee0fe9ac64b11133f33bde153b506d3
emerson <emerson.soares.sc@gmail.com>
2022-05-21 20:56:47 -0300

    commit1

--- contcustos/contabilidade_home.py
+++ contcustos/contabilidade_home.py
@@ -123,6 +123,7 @@ def contc():
             df_dre['% sobre Receitas'] = (df_dre['Valor']*100/total_receitas)
             saldo_total = total_receitas-total_despfixas-total_despvariaveis-total_despfinanceiras-total_investimentos
             st.write(df_dre)
+            st.warning(f'A soma das despesas fixas, variáveis e financeiras correspondem a {(total_despfixas+total_despvariaveis+total_despfinanceiras):.2f}% das receitas totais')
             st.warning(f'O saldo total do seu orçamento é de: {saldo_total}. Isto corresponde a {(saldo_total*100/total_receitas):.2f}% das receitas totais')
 
 
