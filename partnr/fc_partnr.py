5ab161ec9bbd50e92f598eba78dc394acb452d42
emerson <emerson.soares.sc@gmail.com>
2022-06-16 23:04:17 -0300

    commit1

--- partnr/fc_partnr.py
+++ partnr/fc_partnr.py
@@ -21,5 +21,6 @@ def backtest_stock(ticker='BBAS3.SA',benchmark='^BVSP',strategy=''):
     return dados
 
 def layout_page():
+    dados = backtest_stock()
     st.write('Hello World!')   
-    st.write(backtest_stock()) 
+    st.write(dados.plot()) 
