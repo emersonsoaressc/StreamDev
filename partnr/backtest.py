8a53d8b4c67d3dc67a7c090e5bf30e165937fb8c
emerson <emerson.soares.sc@gmail.com>
2022-06-17 00:19:47 -0300

    commit1

--- partnr/backtest.py
+++ partnr/backtest.py
@@ -9,7 +9,7 @@ sns.set()
 
 def layout_page():
     stocks = st.multiselect('Escolha os ativos', ['BBAS3.SA','ABEV3.SA','VALE3.SA'])
-    dados = backtest_stock(f'{[stocks]}')
-    st.write(stocks) 
-    st.write(dados)
+    #dados = backtest_stock()
+    st.write(type(stocks)) 
+    #st.write(dados)
     #st.line_chart(dados)
