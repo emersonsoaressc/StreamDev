14f61f128adfa008667650dae632852cfb6831e0
emerson <emerson.soares.sc@gmail.com>
2022-06-16 23:26:17 -0300

    commit1

--- partnr/backtest.py
+++ partnr/backtest.py
@@ -8,7 +8,8 @@ sns.set()
 
 
 def layout_page():
+    st.selectbox('Escolha os ativos', ['BBAS3.SA','ABEV3.SA','VALE3.SA'])
     dados = backtest_stock()
     st.write('Hello World!') 
     st.write(dados)
-    st.line_chart(dados)
+    #st.line_chart(dados)
