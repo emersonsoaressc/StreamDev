12d3f40736f35d0511f9c41693facf36fd5f8c89
emerson <emerson.soares.sc@gmail.com>
2022-06-16 21:30:50 -0300

    commit1

--- partnr/backtest.py
+++ partnr/backtest.py
@@ -6,9 +6,20 @@ import seaborn as sns
 sns.set()
 
 
-dados = vbt.YFData.download(
-    ['BOVA11.SA','BBAS3.SA'], 
-    missing_index='drop',
-    start='2000-01-01', 
-    interval='1d').get('Close')
-dados = dados/dados.iloc[0]
+def layout_page():
+    st.write('Hello World!')    
+
+class Strategy():
+    def estrategia1():
+        pass
+    pass
+
+def backtest_stock(ticker,benchmark,strategy):
+    dados = vbt.YFData.download(
+        ['BOVA11.SA','BBAS3.SA'], 
+        missing_index='drop',
+        start='2000-01-01', 
+        interval='1d').get('Close')
+    dados = dados/dados.iloc[0]
+    return dados
+
