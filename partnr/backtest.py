7b40e2d6ed0f474d7bb6057f347a21949719c6df
emerson <emerson.soares.sc@gmail.com>
2022-06-16 21:15:42 -0300

    commit1

---
+++ partnr/backtest.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+from numpy import append
+import streamlit as st
+import vectorbt as vbt
+import seaborn as sns
+sns.set()
+
+
+dados = vbt.YFData.download(
+    ['BOVA11.SA','BBAS3.SA'], 
+    missing_index='drop',
+    start='2000-01-01', 
+    interval='1d').get('Close')
+dados = dados/dados.iloc[0]
