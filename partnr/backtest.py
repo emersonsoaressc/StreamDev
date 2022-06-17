785e482b68f1b4f6467fc5591af059e4a41dbfc6
emerson <emerson.soares.sc@gmail.com>
2022-06-16 22:56:02 -0300

    commit1

--- partnr/backtest.py
+++ partnr/backtest.py
@@ -2,26 +2,9 @@ import pandas as pd
 from numpy import append
 import streamlit as st
 import vectorbt as vbt
+from partnr.fc_partnr import layout_page
 import seaborn as sns
 sns.set()
 
-
-def layout_page():
-    st.write('Hello World!')   
-    st.write(backtest_stock()) 
-
-class Strategy():
-    def estrategia1():
-        pass
-    pass
-
-def backtest_stock(ticker,benchmark,strategy):
-    dados = vbt.YFData.download(
-        ['BOVA11.SA','BBAS3.SA'], 
-        missing_index='drop',
-        start='2000-01-01', 
-        interval='1d').get('Close')
-    dados = dados/dados.iloc[0]
-    return dados
 
 layout_page()
---
+++ partnr/fc_partnr.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+from numpy import append
+import streamlit as st
+import vectorbt as vbt
+import seaborn as sns
+sns.set()
+
+
+class Strategy():
+    def estrategia1():
+        pass
+    pass
+
+def backtest_stock(ticker='BBAS3.SA',benchmark='^BVSP',strategy=''):
+    dados = vbt.YFData.download(
+        ['BOVA11.SA','BBAS3.SA'], 
+        missing_index='drop',
+        start='2000-01-01', 
+        interval='1d').get('Close')
+    dados = dados/dados.iloc[0]
+    return dados
+
+def layout_page():
+    dados = backtest_stock()
+    st.write('Hello World!')   
+    st.write(dados) 
