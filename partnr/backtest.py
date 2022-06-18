e7f0e09d3031b2289301beedccfb5b59f958890e
emerson <emerson.soares.sc@gmail.com>
2022-06-18 12:43:31 -0300

    commit1

--- partnr/backtest.py
+++ partnr/backtest.py
@@ -3,6 +3,7 @@ from numpy import append
 import streamlit as st
 import vectorbt as vbt
 from partnr.fc_partnr import *
+import plotly.express as px
 import seaborn as sns
 sns.set()
 
@@ -11,6 +12,6 @@ def layout_page():
     stocks = st.multiselect('Escolha os ativos', ['BBAS3.SA','ABEV3.SA','VALE3.SA'])
     stocks.append('^BVSP')
     dados, retornos = backtest_stock(tickers=stocks)
-    st.write(type(retornos)) 
+    st.write(px.line(retornos)) 
     st.write(dados)
     #st.line_chart(retornos,width=100,height=100)
